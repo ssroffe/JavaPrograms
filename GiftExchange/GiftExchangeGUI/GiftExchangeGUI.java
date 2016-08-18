615c305c5bf4391a1eeb4bde350dc14b64b8e1f0
ssroffe <ssroffe@gmail.com>
2016-08-18 17:22:39 -0400

    edited GiftExchangeGUI.java

--- GiftExchange/GiftExchangeGUI/GiftExchangeGUI.java
+++ GiftExchange/GiftExchangeGUI/GiftExchangeGUI.java
@@ -27,45 +27,82 @@ public class GiftExchangeGUI {
 	//get the list of people's names and store it into an arraylist of strings
 	ArrayList<String> names = new ArrayList<String>();
 
-	//enlarge buttons on inputdialog
+	//enlarge buttons
 	UIManager.put("OptionPane.buttonFont", new FontUIResource(new Font("ARIAL",Font.PLAIN,36)));
-	//UIManager.put("OptionPane.inputBox", new FontUIResource(new Font("ARIAL",Font.PLAIN,36)));	
 
 	
-	//input.nextLine();
+
 	boolean inputNames = true;
 	int count = 1;
-	//boolean confirm = false;
+
+	// Get the list of people
 	do {
 	    JLabel messageLabel = new JLabel("Person "+(count)+" is:");
-	    messageLabel.setPreferredSize(new Dimension(250,300));
+	    messageLabel.setPreferredSize(new Dimension(100,100));
 	    messageLabel.setFont(new Font("Arial",Font.BOLD,36));
 
-	    JPanel panel = new JPanel();
+	    JPanel peoplePanel = new JPanel();
 
-	    panel.add(messageLabel);
-	    
-	    Object[] nextDone = {"Next","Done"};
+	    //panel.add(messageLabel);
 
-	    JTextField textField = new JTextField(30);
+	    Object[] nextDone = {"Next","Done"}; //button labels
+
+	    JTextField textField = new JTextField(30); //input
 	    textField.setPreferredSize(new Dimension(100,50));
 	    textField.setFont(new Font("Arial",Font.PLAIN,30));
-	    panel.add(textField);
+	    //panel.add(textField);
+
+	    JTextArea currentList = new JTextArea(20,10); //Where the names are listed
+	    currentList.setFont(new Font("Arial",Font.PLAIN,24));
+	    
+	    JLabel currentLabel = new JLabel("The current list is: ");
+	    currentLabel.setPreferredSize(new Dimension(400,50));
+	    currentLabel.setFont(new Font("Arial",Font.BOLD,24));
+	    
+	    
+	    for (int i = 0; i < names.size(); i++) {
+		currentList.append(names.get(i)+"\n");
+	    }
+	    
+	    currentList.setEditable(false); //make the list read-only
+	    
+	    JScrollPane scrollPeoplePane = new JScrollPane(currentList);
+	    scrollPeoplePane.setPreferredSize(new Dimension(100,250));
+	    peoplePanel.setLayout(new GridBagLayout());
+	    GridBagConstraints grid = new GridBagConstraints();
+	    grid.gridx = 0;
+	    grid.gridy = 0;
+	    grid.anchor = GridBagConstraints.WEST;
+
+	    peoplePanel.add(currentLabel,grid);
+	    grid.fill = GridBagConstraints.HORIZONTAL;
+	    grid.gridy++;
+	    peoplePanel.add(currentList,grid);
+	    grid.fill = GridBagConstraints.HORIZONTAL;
+	    grid.gridy++;
+	    peoplePanel.add(messageLabel,grid);
+	    grid.fill = GridBagConstraints.HORIZONTAL;
+	    grid.gridy++;
+	    peoplePanel.add(textField,grid);
+	    
+	    
 	    
 	    int optionPressed = JOptionPane.showOptionDialog(null,
-							    panel, //message
-							    "Gift Exchange", //title
-							    JOptionPane.YES_NO_OPTION,
-							    JOptionPane.QUESTION_MESSAGE,
-							    null, //no custom icon
-							    nextDone, //title of buttons
-							    textField //default button
+							     peoplePanel, //message
+							     "Gift Exchange", //title
+							     JOptionPane.YES_NO_OPTION,
+							     JOptionPane.QUESTION_MESSAGE,
+							     null, //no custom icon
+							     nextDone, //title of buttons
+							     textField //default button
 							    );
-
+	    
+	    // Add the person to the list whilst checking for errors
 	    String person; 
 	    if (optionPressed == JOptionPane.YES_OPTION) {
 		person = textField.getText();
 		if (person.length() == 0) {
+		    //if Next was pushed but nothing was entered
 		    JLabel errMessage = new JLabel("You need to input a person\'s name");
 
 		    errMessage.setFont(new Font("Arial",Font.BOLD,32));
@@ -83,6 +120,7 @@ public class GiftExchangeGUI {
 		}
 	    }
 	    else if (optionPressed == JOptionPane.NO_OPTION){
+		// Done pushed - If nothing entered on last person, ignore that entry
 		person = textField.getText();
 		if (person.length() == 0) {
 		    inputNames = false;
@@ -93,10 +131,11 @@ public class GiftExchangeGUI {
 		}
 	    }
 	    else if (optionPressed == JOptionPane.CLOSED_OPTION) {
+		//exiting by closing the window
 		System.exit(0);
 	    }
 
-	    
+	    // Check the names in the list
 	    if (!inputNames) {
 		Object[] confirmList = {"Yes","No"};
 		
@@ -104,10 +143,13 @@ public class GiftExchangeGUI {
 		listPanel.setPreferredSize(new Dimension(400,400));
 		JLabel listLabel = new JLabel("The names in the list are: ");
 		listLabel.setPreferredSize(new Dimension(400,50));
-		listLabel.setFont(new Font("Arial",Font.BOLD,24));		
+		listLabel.setFont(new Font("Arial",Font.BOLD,24));
 		
+		JLabel correctLabel = new JLabel("Is this correct?");
+		correctLabel.setPreferredSize(new Dimension(400,50));
+		correctLabel.setFont(new Font("Arial",Font.BOLD,24));	     
 
-		JTextArea listOfPeople = new JTextArea(25,25);
+		JTextArea listOfPeople = new JTextArea(25,25); //Where the names are listed
 
 		listOfPeople.setFont(new Font("Arial",Font.PLAIN,24));
 		
@@ -115,7 +157,7 @@ public class GiftExchangeGUI {
 		    listOfPeople.append(names.get(i)+"\n");
 		}
 
-		listOfPeople.setEditable(false);
+		listOfPeople.setEditable(false); //make the list read-only
 
 		JScrollPane scrollPane = new JScrollPane(listOfPeople);
 		scrollPane.setPreferredSize(new Dimension(100,250));
@@ -125,11 +167,15 @@ public class GiftExchangeGUI {
 		gbc.gridy = 0;
 		gbc.anchor = GridBagConstraints.WEST;
 
+		//format the window
 		listPanel.add(listLabel,gbc);
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		gbc.gridy++;
 		listPanel.add(scrollPane,gbc);
-		
+		gbc.fill= GridBagConstraints.HORIZONTAL;
+		gbc.gridy++;
+		listPanel.add(correctLabel,gbc);
+				
 		
 		int listPressed = JOptionPane.showOptionDialog(null,
 							       listPanel, //message
@@ -142,10 +188,10 @@ public class GiftExchangeGUI {
 							       );
 
 		if (listPressed == JOptionPane.YES_OPTION) {
-		    
+		    //if yes was pushed, just continue on
 		}
 		else if (listPressed == JOptionPane.NO_OPTION){
-		    //confirm = true;
+		    //start from scratch if no was pushed
 		    inputNames = true;
 		    names.clear();
 		    count = 1;
