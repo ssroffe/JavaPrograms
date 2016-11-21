957bd76f73daf1c60a52470266c558cef7a7c9d3
ssroffe <ssroffe@gmail.com>
2016-11-20 22:40:15 -0500

    Automated emailing in GiftExchangeGUI

--- GiftExchange/GiftExchangeGUI/GiftExchangeGUI.java
+++ GiftExchange/GiftExchangeGUI/GiftExchangeGUI.java
@@ -5,11 +5,16 @@
 package GiftExchangeGUI;
 
 import java.util.*;
+import java.util.regex.*;
+import java.util.logging.*;
 import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.plaf.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.Message.*;
 
 public class GiftExchangeGUI {
     
@@ -18,7 +23,7 @@ public class GiftExchangeGUI {
 	those people. The outputs are files that are named for the buyer
 	containing a string that explains who that person is buying for.
     */
-    public static void main(String[] args) throws FileNotFoundException {	
+    public static void main(String[] args) throws FileNotFoundException,AddressException,MessagingException {	
 	
 	Scanner input = new Scanner(System.in);
 
@@ -26,30 +31,40 @@ public class GiftExchangeGUI {
 	inputFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	//get the list of people's names and store it into an arraylist of strings
 	ArrayList<String> names = new ArrayList<String>();
-
-	//enlarge buttons
-	UIManager.put("OptionPane.buttonFont", new FontUIResource(new Font("ARIAL",Font.PLAIN,36)));
-
+	ArrayList<String> emails = new ArrayList<String>();
 	
+	//enlarge buttons
+	UIManager.put("OptionPane.buttonFont", new FontUIResource(new Font("ARIAL",Font.PLAIN,36)));	
 
 	boolean inputNames = true;
 	int count = 1;
 
+	String[] userpass = getEmailPswd();
+	String username = userpass[0];
+	String password = userpass[1];
+	
 	// Get the list of people
 	do {
+
+	    JLabel emailLabel = new JLabel("Person " + (count) + "'s email is (No email will make a file for the person):");
+	    emailLabel.setPreferredSize(new Dimension(100,100));
+	    emailLabel.setFont(new Font("Arial",Font.BOLD,34));
+	    
 	    JLabel messageLabel = new JLabel("Person "+(count)+" is:");
 	    messageLabel.setPreferredSize(new Dimension(100,100));
 	    messageLabel.setFont(new Font("Arial",Font.BOLD,36));
 
 	    JPanel peoplePanel = new JPanel();
 
-	    //panel.add(messageLabel);
-
 	    Object[] nextDone = {"Next","Done"}; //button labels
 
-	    JTextField textField = new JTextField(30); //input
+	    JTextField textField = new JTextField(40); //input
 	    textField.setPreferredSize(new Dimension(100,50));
 	    textField.setFont(new Font("Arial",Font.PLAIN,30));
+
+	    JTextField emailField = new JTextField(40); //email input
+	    emailField.setPreferredSize(new Dimension(100,50));
+	    emailField.setFont(new Font("Arial",Font.PLAIN,30));
 	    //panel.add(textField);
 
 	    JTextArea currentList = new JTextArea(20,10); //Where the names are listed
@@ -61,7 +76,7 @@ public class GiftExchangeGUI {
 	    
 	    
 	    for (int i = 0; i < names.size(); i++) {
-		currentList.append(names.get(i)+"\n");
+		currentList.append(names.get(i)+"\t" + emails.get(i)+"\n");
 	    }
 	    
 	    currentList.setEditable(false); //make the list read-only
@@ -84,6 +99,12 @@ public class GiftExchangeGUI {
 	    grid.fill = GridBagConstraints.HORIZONTAL;
 	    grid.gridy++;
 	    peoplePanel.add(textField,grid);
+	    grid.fill = GridBagConstraints.HORIZONTAL;
+	    grid.gridy++;
+	    peoplePanel.add(emailLabel,grid);
+	    grid.fill = GridBagConstraints.HORIZONTAL;
+	    grid.gridy++;
+	    peoplePanel.add(emailField,grid);
 	    
 	    
 	    
@@ -98,9 +119,12 @@ public class GiftExchangeGUI {
 							    );
 	    
 	    // Add the person to the list whilst checking for errors
-	    String person; 
+	    String person;
+	    String email;
 	    if (optionPressed == JOptionPane.YES_OPTION) {
 		person = textField.getText();
+		email = emailField.getText();
+		
 		if (person.length() == 0) {
 		    //if Next was pushed but nothing was entered
 		    JLabel errMessage = new JLabel("You need to input a person\'s name");
@@ -113,20 +137,47 @@ public class GiftExchangeGUI {
 						  JOptionPane.WARNING_MESSAGE
 						  );
 		}
+		
+		else if ( !isValidEmail(email) && email.length() != 0 ) {
+		    JLabel errMessage = new JLabel("That is not a valid email address");
+		    errMessage.setFont(new Font("Arial", Font.BOLD, 32));
+		    JOptionPane.showMessageDialog(null,
+						  errMessage,
+						  "Invalid email",
+						  JOptionPane.WARNING_MESSAGE
+						  );
+		}
+
 		else {
 		    count++;
 		    names.add(person);
+		    if (email.length() == 0) {
+			emails.add("");
+		    }
+		    else {
+			emails.add(email);
+		    }
+		    
 		    inputNames = true;
 		}
 	    }
 	    else if (optionPressed == JOptionPane.NO_OPTION){
 		// Done pushed - If nothing entered on last person, ignore that entry
 		person = textField.getText();
+		email = emailField.getText();
 		if (person.length() == 0) {
 		    inputNames = false;
 		}
 		else {
 		    names.add(person);
+
+		    if (email.length() == 0) {
+			emails.add("");
+		    }
+		    else {
+			emails.add(email);
+		    }
+
 		    inputNames = false;
 		}
 	    }
@@ -154,7 +205,7 @@ public class GiftExchangeGUI {
 		listOfPeople.setFont(new Font("Arial",Font.PLAIN,24));
 		
 		for (int i = 0; i < names.size(); i++) {
-		    listOfPeople.append(names.get(i)+"\n");
+		    listOfPeople.append(names.get(i)+" - "+ emails.get(i)+"\n");
 		}
 
 		listOfPeople.setEditable(false); //make the list read-only
@@ -194,6 +245,7 @@ public class GiftExchangeGUI {
 		    //start from scratch if no was pushed
 		    inputNames = true;
 		    names.clear();
+		    emails.clear();
 		    count = 1;
 		}
 		else if (listPressed == JOptionPane.CLOSED_OPTION) {
@@ -223,7 +275,7 @@ public class GiftExchangeGUI {
 	    }
 	} while (getSelf);
 
-	JLabel doneLabel = new JLabel("Done. File names are the buyer\n File contains who buyer is buying for.");
+	JLabel doneLabel = new JLabel("Done. Files emailed. If there was no email a file was created containing who that person is buying for.");
 
 	doneLabel.setFont(new Font("Arial",Font.PLAIN,34));
 	JOptionPane.showMessageDialog(null,
@@ -231,22 +283,200 @@ public class GiftExchangeGUI {
 				      "Done Matching",
 				      JOptionPane.INFORMATION_MESSAGE
 				      );
-       	
-	//write files
+	
+	//write files and email people
+	EmailPeople(userpass,names,emails,exchange);
+	System.exit(0);
+    }
+
+    
+    public static boolean isValidEmail(String email){
+	//Check if valid email address
+	
+        Pattern pattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
+        Matcher mat = pattern.matcher(email);
+
+        if(mat.matches()){
+            return true;
+        }
+	else{
+            return false;
+        }
+    }
+
+    public static String[] getEmailPswd() {
+	boolean getEmail = true;
+	String[] tmp = {"",""};
+	Object[] nextDone = {"OK","Cancel"}; //button labels
+	do { 
+
+	    JPanel userPanel = new JPanel();
+	    
+	    JTextField username = new JTextField(40);
+	    username.setPreferredSize(new Dimension(100,50));
+	    username.setFont(new Font("Arial", Font.PLAIN,30));
+	    
+	    JPasswordField passwd = new JPasswordField(40);
+	    passwd.setPreferredSize(new Dimension(100,50));
+	    passwd.setFont(new Font("Arial", Font.PLAIN,30));
+	    
+	    JLabel userLabel = new JLabel("Email Username: ");
+	    userLabel.setPreferredSize(new Dimension(400,50));
+	    userLabel.setFont(new Font("Arial",Font.BOLD,24));
+	    
+	    JLabel passLabel = new JLabel("Password: ");
+	    passLabel.setPreferredSize(new Dimension(400,50));
+	    passLabel.setFont(new Font("Arial",Font.BOLD,24));
+	    
+	    userPanel.setLayout(new GridBagLayout());
+	    GridBagConstraints grid = new GridBagConstraints();
+	    grid.gridx = 0;
+	    grid.gridy = 0;
+	    grid.anchor = GridBagConstraints.WEST;
+	    
+	    userPanel.add(userLabel,grid);
+	    grid.fill = GridBagConstraints.HORIZONTAL;
+	    grid.gridy++;
+	    userPanel.add(username,grid);
+	    grid.fill = GridBagConstraints.HORIZONTAL;
+	    grid.gridy++;
+	    userPanel.add(passLabel,grid);
+	    grid.fill = GridBagConstraints.HORIZONTAL;
+	    grid.gridy++;
+	    userPanel.add(passwd,grid);
+
+	    int listPressed = JOptionPane.showOptionDialog(null,
+							   userPanel, //message
+							   "Gift Exchange - Host Email", //title
+							   JOptionPane.YES_NO_OPTION,
+							   JOptionPane.INFORMATION_MESSAGE,
+							   null, //no custom icon
+							   nextDone, //title of buttons
+							   username //default button
+							   );
+	    
+	    if (listPressed == JOptionPane.YES_OPTION) {
+
+		if (username.getText().length() == 0 || !isValidEmail(username.getText())) {
+		    //if Next was pushed but nothing was entered
+		    JLabel errMessage = new JLabel("You need to input a valid email address");
+
+		    errMessage.setFont(new Font("Arial",Font.BOLD,32));
+		    
+		    JOptionPane.showMessageDialog(null,
+						  errMessage,
+						  "Invalid Host email",
+						  JOptionPane.WARNING_MESSAGE
+						  );
+		    getEmail = true;
+		}
+		
+		else if ( passwd.getText().length() == 0 ) {
+		    JLabel errMessage = new JLabel("You need to input a password.");
+
+		    errMessage.setFont(new Font("Arial",Font.BOLD,32));
+		    
+		    JOptionPane.showMessageDialog(null,
+						  errMessage,
+						  "No password inputted",
+						  JOptionPane.WARNING_MESSAGE
+						  );
+		    getEmail = true;
+		}
+		
+		else {
+		    getEmail = false;
+		    String user = username.getText();
+		    String pass = passwd.getText();
+		    String[] userpass = {user,pass};
+		    return userpass;
+		}
+	    }
+	    else if (listPressed == JOptionPane.NO_OPTION){
+		//cancel == cancel the program
+		System.exit(0);
+	    }
+	    else if (listPressed == JOptionPane.CLOSED_OPTION) {
+		System.exit(0);
+	    }	    
+	    
+	} while (getEmail);
+	return tmp;
+	
+    }
+
+
+    public static void EmailPeople(String[] userpass, ArrayList<String> names, ArrayList<String> emails, ArrayList<Integer> exchange) throws FileNotFoundException,AddressException, MessagingException{
 	try {
 	    String pathToJar = GiftExchangeGUI.class.getProtectionDomain().getCodeSource().getLocation().getPath();
 	    String path = new File(pathToJar).getParentFile().getPath();
-	    
-	    for (int i = 0; i<names.size(); i++) {
-		PrintWriter output = new PrintWriter(path+"/"+names.get(i)+".txt");
-		//Map number in shuffled integer array to original names array.
-		output.print("You are buying for "+names.get(exchange.get(i))+".");
-		output.close();
+	    for (int i = 0; i < names.size(); i++) {
+		if (emails.get(i) == "") {
+		    //write files for people who don't have emails
+		    PrintWriter output = new PrintWriter(path+"/"+names.get(i)+".txt");
+		    //Map number in shuffled integer array to original names array.
+		    output.print("You are buying for "+names.get(exchange.get(i))+".");
+		    output.close();
+		}
+		else {
+		    //email the people
+
+		    Properties props = System.getProperties();
+		    props.put("mail.smtp.starttls.enable", "true");
+		    props.put("mail.smtp.host", "smtp.gmail.com");
+		    props.put("mail.smtp.auth", "true");
+		    props.put("mail.smtp.port", "465");//"465"); // smtp port
+		    props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		    Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			    protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userpass[0], userpass[1]);
+			    }
+			});
+		    /*
+		      Authenticator auth = new Authenticator() {
+			    
+			    @Override
+			    protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userpass[0], userpass[1]);
+			    }
+			};
+		    Session session = Session.getDefaultInstance(props, auth);
+		    */
+		    MimeMessage msg = new MimeMessage(session);
+		    
+		    msg.setFrom(new InternetAddress(userpass[0]));
+		    msg.setSubject("Gift Exchange Person Output");
+		   
+		    msg.setRecipient(RecipientType.TO, new InternetAddress(emails.get(i)));
+		    //add atleast simple body
+
+		    MimeBodyPart body = new MimeBodyPart();
+		    body.setText("You are buying for "+names.get(exchange.get(i))+".");
+		    
+		    
+		    Multipart multipart = new MimeMultipart();
+		    multipart.addBodyPart(body);
+		    
+		    msg.setContent(multipart);
+		    Transport.send(msg);
+		    
+		}
+		
 	    }
 	}
 	catch (IOException ioe) {
 	    System.out.println("There was an IO exception.");
 	}
-	System.exit(0);
+	catch (Exception e) {
+	    e.printStackTrace();
+	    System.out.println("There was an error");
+	}
     }
 }
+	//catch (AddressException ex) {
+	//   Logger.getLogger(NewClass.class.getName()).log(Level.SEVERE, null, ex);
+	//} catch (MessagingException ex) {
+	//    Logger.getLogger(NewClass.class.getName()).log(Level.SEVERE, null, ex);
+	//}
+	
+
